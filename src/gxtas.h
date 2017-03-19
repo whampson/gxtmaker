fb520f6d4746f4effcff2affb8562d71d0f0b42f
Wes Hampson <thehambone93@gmail.com>
2017-03-18 22:31:23 -0500

    Add gxtas.h

---
+++ src/gxtas.h
@@ -0,0 +1,39 @@
+/*
+* Copyright (c) 2017 Wes Hampson <thehambone93@gmail.com>
+*
+* Licensed under the MIT License. See LICENSE at top level directory.
+*/
+
+#ifndef _GXTAS_H_
+#define _GXTAS_H_
+
+#define GXTAS_VERSION_MAJOR 0
+#define GXTAS_VERSION_MINOR 0
+#define GXTAS_VERSION_PATCH 0
+#define GXTAS_VERSION_BUILD ""  /* Additional build info goes here. */
+
+#define GXTAS_APP_NAME  "gxtas"
+#define GXTAS_APP_MOTTO "The GTA Text Assembler"
+
+#define GXTAS_HELP_MESSAGE \
+"Usage: " GXTAS_APP_NAME " [options] file\n\
+\nOptions:\n\
+    --help      show this help menu and exit\n\
+    --version   display program version information and exit"
+
+#define GXTAS_COPYRIGHT_NOTICE \
+"Copyright (c) 2017 Wes Hampson <thehambone93@gmail.com>"
+
+#define GXTAS_LICENSE_NOTICE \
+"This software is licensed under the MIT License."
+
+#define GXTAS_WARRANTY_NOTICE \
+"THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n\
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n\
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n\
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n\
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n\
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n\
+SOFTWARE."
+
+#endif /* _GXTAS_H_ */
