6f1d549cd790db2af62594366ee815c58d0cc762
Wes Hampson <thehambone93@gmail.com>
2017-03-27 00:53:09 -0500

    Move hex_dump() to io.c

--- src/compiler.c
+++ src/compiler.c
@@ -8,12 +8,9 @@
 #include <stdio.h>
 
 #include "compiler.h"
+#include "io.h"
 
 #define CHUNK_SIZE 1024
-
-#define NUM_BYTES_PER_LINE 16
-#define NUM_CHARS_PER_BYTE 3
-
 #define GXT_KEY_MAX_LEN 8
 
 #define MIN(a, b) ((a < b) ? (a) : (b))
@@ -32,7 +29,7 @@ struct compiler_state
     int num_keys;
 };
 
-static void hex_dump(const void *buf, size_t size);
+
 static int compile_chunk(const char *chunk, size_t chunk_size,
                          struct compiler_state *state);
 static bool is_whitespace(char c);
@@ -151,69 +148,7 @@ static int compile_chunk(const char *chunk, size_t chunk_size,
     return 0;
 }
 
-bool is_whitespace(char c)
+static bool is_whitespace(char c)
 {
     return c == ' ' || c == '\t';
-}
-
-/**
- * Dumps the contents of a buffer to stdout.
- */
-static void hex_dump(const void *buf, size_t size)
-{
-    const char *b = (char *) buf;
-
-    char c;
-    int i;
-    int row_len;
-    int pad_len;
-    int off = 0;
-
-    do
-    {
-        /* Print current offset in buffer */
-        printf("%04x  ", off);
-
-        /* Print a line of bytes in hexadecimal */
-        row_len = MIN(NUM_BYTES_PER_LINE, size - off);
-        for (i = 0; i < row_len; i++)
-        {
-            printf("%02x", b[off + i] & 0xFF);
-            if (i != row_len - 1)
-            {
-                printf(" ");
-            }
-        }
-
-        /* Print padding spaces */
-        pad_len = (NUM_BYTES_PER_LINE - row_len) * NUM_CHARS_PER_BYTE;
-        for (i = 0; i < pad_len; i++)
-        {
-            printf(" ");
-        }
-        printf("  ");
-
-        /* Print bytes in ASCII representation */
-        /* Replace non-printing characters with '.' */
-        for (i = 0; i < row_len; i++)
-        {
-            c = b[off + i];
-            if (c < 0x20 || c > 0x7F)
-            {
-                printf(".");
-            }
-            else
-            {
-                printf("%c", c);
-            }
-        }
-
-        /* Increase offset and print newline */
-        off += row_len;
-        if (off < size)
-        {
-            printf("\n");
-        }
-    }
-    while (off < size);
 }
